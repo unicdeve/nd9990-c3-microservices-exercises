e4f715a558b65a3c39091ab937722b0b49aaa72d
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-10-30 20:01:21 +0100

    feat: code restructure

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=unicdev
+export POSTGRES_PASSWORD=Password1234
+export POSTGRES_HOST=cdr1.cwhil3ijr85n.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramunicdev
+export AWS_BUCKET=unicdev-481932773375-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=njkndskjnjknjkndsjknj
+export URL=http://localhost:8100
